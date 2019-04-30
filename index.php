d2fcf514e4f315c31f841786e7483f94bf4b369b
Michael Jordan <mrmikeman38@gmail.com>
2019-04-30 14:21:32 +1000

    Removed sample text

--- index.php
+++ index.php
@@ -47,10 +47,6 @@
       <div class="headingContainer">
         <h1 style="background-color: #E8E9EB" class="mainHeading"> DB Zenart Creations </h1>
       </div>
-
-      <div class="container">
-        <p class="text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
   </section>
 
 
